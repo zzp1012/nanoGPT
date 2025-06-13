e5d520a10b5c76504c4346ad24e703bed7616e5a
zzp1012 <1012zzphh@gmail.com>
2025-06-13 17:21:57 +0800

    update: add llama here

--- plot_fisher.py
+++ plot_fisher.py
@@ -6,24 +6,36 @@ import seaborn as sns
 from pathlib import Path
 
 # define Gloabl variables
-FISHER_MAT_DIR = "/minimax-dialogue/users/Zp/nanoGPT/out/minimax_dense_ratio/fisher_mat"
-SAVE_PATH = "/minimax-dialogue/users/Zp/nanoGPT/out/dense/fisher_mat_plot"
-ITERATIONS = [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+FISHER_MAT_DIR = "/jfs-dialogue-mmos02-rs02/users/Zp/nanoGPT/out/llama_ratio/0.25B/baseline/fisher_mat"
+SAVE_PATH = "/jfs-dialogue-mmos02-rs02/users/Zp/nanoGPT/out/fisher_mat_plot/llama/0.25B/baseline/"
+ITERATIONS = [2000]
 
 def preprocess_fisher_mat(fisher_mat: dict):
     """
     Preprocess the fisher matrix to make it a square matrix.
     """
-    name_dict = {
-        "Embed": ['embed_tokens.weight'],
-        "QK": ['self_attn.q_proj.weight', 'self_attn.k_proj.weight'],
-        "MLP": ['w1.weight', 'w2.weight', 'w3.weight'],
-        "VO": ['self_attn.v_proj.weight', 'self_attn.o_proj.weight'],
-        # "Router": ['block_sparse_moe.gate.weight'], 
-        "LayerNorm": ['input_layernorm.weight', 'post_attention_layernorm.weight'],
-        "RMSNorm": ['model.norm.weight'],
-        "Head": ['lm_head.weight']
-    }
+    if "llama" in FISHER_MAT_DIR:
+        name_dict = {
+            'Embed': ['embed_tokens.weight'],
+            'QK': ['self_attn.q_proj.weight', 'self_attn.k_proj.weight'], 
+            'FFN': ['mlp.gate_proj.weight', 'mlp.up_proj.weight', 'mlp.down_proj.weight'],
+            'Head': ['lm_head.weight'], 
+            'VO': ['self_attn.v_proj.weight', 'self_attn.o_proj.weight'], 
+            'Norm': ['input_layernorm.weight', 'post_attention_layernorm.weight']
+        }
+    elif "minimax" in FISHER_MAT_DIR:
+        name_dict = {
+            "Embed": ['embed_tokens.weight'],
+            "QK": ['self_attn.q_proj.weight', 'self_attn.k_proj.weight'],
+            "MLP": ['w1.weight', 'w2.weight', 'w3.weight'],
+            "VO": ['self_attn.v_proj.weight', 'self_attn.o_proj.weight'],
+            # "Router": ['block_sparse_moe.gate.weight'], 
+            "LayerNorm": ['input_layernorm.weight', 'post_attention_layernorm.weight'],
+            "RMSNorm": ['model.norm.weight'],
+            "Head": ['lm_head.weight'],
+        }
+    else:
+        raise ValueError(f"Unknown model: {FISHER_MAT_DIR}")
     
     # concatenate the fisher matrix for each parameter group
     preprocessed_fisher_mat = {}
@@ -43,10 +55,10 @@ def random_selection(fisher_mat: dict, num_selected: int, eps: float = 1e-14):
     """
     selected_fisher_mat = {}
     for block_type, fisher in fisher_mat.items():
-        attained_index = np.where(fisher > eps)[0]
-        print(f"{block_type}: {attained_index.size}")
-        selected_index = np.random.choice(attained_index, size=num_selected, replace=attained_index.size < num_selected)
-        selected_fisher_mat[block_type] = fisher[selected_index]
+        size = fisher.size
+        print(f"{block_type}: {size}")
+        selected_index = np.random.choice(size, size=num_selected, replace=size <= num_selected)
+        selected_fisher_mat[block_type] = np.maximum(fisher[selected_index], eps)
     return selected_fisher_mat
 
 
