0c6ad4f6eda0a16dd82df83b4db31218f5fba262
zzp1012 <1012zzphh@gmail.com>
2025-07-05 22:03:21 +0800

    update: change to new dataset

--- model_minimax.py
+++ model_minimax.py
@@ -1,7 +1,7 @@
 from transformers import AutoConfig
 from modeling_minimax_text_01 import MiniMaxText01ForCausalLM
 
-def build_minimax_model(model_name: str):
+def build_minimax_model(model_name: str, block_size: int = 1024, vocab_size: int = 50304):
     """
     Build a MiniMax model from a given model name.
     """
@@ -13,8 +13,8 @@ def build_minimax_model(model_name: str):
         config.intermediate_size = 4096
         config.num_attention_heads = 8
         config.num_key_value_heads = 8
-        config.max_position_embeddings = 1024
-        config.vocab_size = 50304
+        config.max_position_embeddings = block_size
+        config.vocab_size = vocab_size
         config.num_local_experts = 1
         config.num_experts_per_tok = 1
         config.attn_type_list = [1] * 8
@@ -27,8 +27,8 @@ def build_minimax_model(model_name: str):
         config.intermediate_size = 4096
         config.num_attention_heads = 8
         config.num_key_value_heads = 8
-        config.max_position_embeddings = 1024
-        config.vocab_size = 50304
+        config.max_position_embeddings = block_size
+        config.vocab_size = vocab_size
         config.num_local_experts = 8
         config.num_experts_per_tok = 2
         config.attn_type_list = [1] * 8
